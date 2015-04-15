1b87aff73500dc45212450b37d937382a5f283f4
Spinaldash <blueavenger@gmail.com>
2015-04-15 11:12:24 -0700

    pushing tasks to firebase

--- client/index.js
+++ client/index.js
@@ -9,8 +9,38 @@ function init(){
   root = new Firebase('https://mj-todo.firebaseio.com/');
   user = root.child('user');
   tasks = root.child('tasks');
+  user.on('value', userChanged);
+  $('#create-task').on('click', createTask);
+}
+
+function createTask(){
+  var title = $('#title').val();
+  var dueDate = $('#due-date').val();
+  dueDate = new Date(dueDate); // turns it into a date object from a string
+  dueDate = dueDate.getTime(); // turns it into a number from a date
+  var priority = $('#priority').val();
+  var isComplete = false;
+  var createdAt = Firebase.ServerValue.TIMESTAMP;
+
+  var task = {
+    title: title,
+    dueDate: dueDate,
+    priority: priority,
+    isComplete: isComplete,
+    createdAt: createdAt
+  };
+
+  tasks.push(task);
+}
+
+function userChanged(snapshot){
+  var name = snapshot.val();
+  $('#header').text('Welcome to Todo : ' + name);
+  console.log(snapshot.val());
 }
 
 function setName(){
   var name = $('#name').val();
+  $('#name').val('');
+  user.set(name);
 }
