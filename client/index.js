22bddff57dea7d969d57e7f586d04f0639dc6e73
Spinaldash <blueavenger@gmail.com>
2015-04-15 10:11:11 -0700

    installing first Firebase

--- client/index.js
+++ client/index.js
@@ -1,2 +1,16 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+var root, user, tasks;
+
+function init(){
+  $('#set-name').click(setName);
+  root = new Firebase('https://mj-todo.firebaseio.com/');
+  user = root.child('user');
+  tasks = root.child('tasks');
+}
+
+function setName(){
+  var name = $('#name').val();
+}
